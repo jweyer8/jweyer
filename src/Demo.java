fc58aacf83b7a4d5379db918536978b3b8b25a7d
jweyer8 <59973736+jweyer8@users.noreply.github.com>
2020-01-16 10:50:22 -0800

    change array

--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
     public static void main(String[] args){
         System.out.println("Hello World");
-        int[] a = {1,2,3,4,5};
+        int[] a = {1,2,3,4,5,6,7,8,9};
         for(int num : a){
             System.out.println(num);
         }
