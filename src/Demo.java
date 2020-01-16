7ffe5d6aaa1244e5177cbfe13d85117bd80cda59
jweyer8 <jweyermtb8@gmail.com>
2020-01-16 10:03:34 -0800

    loop

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,9 @@
 public class Demo {
     public static void main(String[] args){
         System.out.println("Hello World");
+        int[] a = {1,2,3,4,5};
+        for(int num : a){
+            System.out.println(num);
+        }
     }
 }
